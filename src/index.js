7a6d3f5d275a9b5adde9a6a37a78521b24d6bd48
Gustavo Silva <silvanaranjogustavoandres@gmail.com>
2022-10-18 08:46:11 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(`${__dirname}/data/db.json`);
+const middlewares = jsonServer.defaults();
+const cors = require('cors')
+
+server.use(cors());
+
+const port = process.env.PORT || 4000;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+server.use(router);
+server.use(middlewares)
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
